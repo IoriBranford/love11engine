261df8528f44758b647ded51838c84e9a8e79643
IoriBranford <ioribranford@gmail.com>
2019-11-16 17:28:19 -0800

    Replace old tiled code.

--- game/tiled.lua
+++ game/tiled.lua
@@ -11,158 +11,103 @@ local LFS = love.filesystem
 local LG = love.graphics
 local LM = love.math
 
-local function dirname(fullpath)
-	return fullpath:match'(.*/)[^/]*$'
-end
-
 local Tiled = {}
-
-local Parsers = {}
 
 local loaded = {}
 
-function Parsers.properties(doc)
-	return doc:match([[
-<properties>
-	{{<property name='$_' type='$type' value='$value'/>}}
-</properties>
-	]])
-end
-
-function Parsers.text(doc)
-	return doc:match([[
-<text fontfamily='$fontfamily' pixelsize='$pixelsize' wrap='$wrap'
-color='$color' bold='$bold' italic='$italic' underline='$underline'
-strikeout='$strikeout' kerning='$kerning' halign='$halign'
-valign='$valign'>$text</text>
-	]])
-end
-
-function parseChildren(tbl, doc, insert)
-	insert = insert or function(tbl, tag, parsed)
-		tbl[tag] = parsed
+local load = {}
+setmetatable(load, {
+	__index = function()
+		return function(node)
+			return node
+		end
 	end
-	for child in doc:childtags() do
-		local tag = child.tag
-		local parser = Parsers[tag]
-		local childtbl
-		local parsed = parser and parser(child)-- or child
-		insert(tbl, tag, parsed)
+})
+
+function load.properties(node, parent, dir)
+	for i = 1, #node do
+		local property = node[i]
+		local ptype = property.type
+		local value = property.value or property.default
+		if value and ptype == "file" then
+			value = dir..value
+		end
+		parent[property.name] = value
 	end
 end
 
-function Parsers.object(doc)
-	local object = doc:match([[
-<object id='$id' name='$name' type='$type' x='$x' y='$y'
-width='$width' height='$height' rotation='$rotation' gid='$gid'
-visible='$visible' template='$template'/>
-	]])
+function load.objecttype(node, parent, dir)
+	load.properties(node, node, dir)
+	for i = #node, 1, -1 do
+		node[i] = nil
+	end
+	return node
+end
 
-	parseChildren(object, doc, function(object, tag, parsed)
-		if tag == "ellipse" then
-			object.ellipse = true
-		elseif tag == "point" then
-			object.point = true
-		elseif tag == "polygon" then
-			object.polygon = child.attr.points
-		elseif tag == "polyline" then
-			object.polyline = child.attr.points
-		elseif parsed then
-			object[tag] = parsed
+function load.objecttypes(node, parent, dir)
+	for i = #node, 1, -1 do
+		local objecttype = node[i]
+		node[objecttype.name] = objecttype
+		node[i] = nil
+	end
+	return node
+end
+
+function load.ellipse(node, parent, dir)
+	parent.ellipse = true
+end
+
+function load.point(node, parent, dir)
+	parent.point = true
+end
+
+function load.polygon(node, parent, dir)
+	parent.polygon = node.points
+end
+
+function load.polyline(node, parent, dir)
+	parent.polyline = node.points
+end
+
+function load.text(node, parent, dir)
+	node.string = node[1]
+	node[1] = nil
+	local fontfamily = node.fontfamily
+	local file = dir..fontfamily
+	if node.bold then
+		file = file.."bold"
+	end
+	if node.italic then
+		file = file.."italic"
+	end
+	if node.underline then
+		file = file.."underline"
+	end
+	local pixelsize = node.pixelsize or 16
+	local ttfsize = file..".ttf"..pixelsize
+	local fnt = file..pixelsize..".fnt"
+	local font = loaded[fnt] or loaded[ttfsize]
+	if not font then
+		font = LFS.getInfo(fnt) and LG.newFont(fnt)
+		if font then
+			loaded[fnt] = font
 		end
-	end)
-
-	return object
-end
-
-function Parsers.objectgroup(doc)
-	local objectgroup = doc:match([[
-<objectgroup id='$id' name='$name' color='$color' x='$x' y='$y' width='$width'
-height='$height' opacity='$opacity' visible='$visible' offsetx='$offsetx'
-offsety='$offsety' draworder='$draworder'/>
-]])
-	parseChildren(objectgroup, doc, function(objectgroup, tag, parsed)
-		if parsed then
-			if tag == "object" then
-				objectgroup[#objectgroup+1] = parsed
-			else
-				objectgroup[tag] = parsed
-			end
+	end
+	if not font then
+		local ttf = file..".ttf"
+		font = LFS.getInfo(ttf) and LG.newFont(ttf, pixelsize)
+		if font then
+			loaded[ttfsize] = font
 		end
-	end)
-	return objectgroup
-end
-
-function Parsers.image(doc)
-	return doc:match([[
-<image format='$format' id='$id' source='$source'
-trans='$trans' width='$width' height='$height'/>
-]])
-end
-
-function Parsers.animation(doc)
-	return doc:match([[
-	<animation>
-		{{<frame tileid='$tileid' duration='$duration'/>}}
-	</animation>
-	]])
-end
-
-function Parsers.tile(doc)
-	local tile = doc:match([[
-<tile id='$id' type='$type' terrain='$terrain' probability='$probability'/>
-	]])
-	parseChildren(tile, doc)
-	return tile
-end
-		-- ]]..Properties..[[
--- ]]..Properties..Image..ObjectGroup..[[
-
---function Parsers.terrain(doc)
---	local terrain = doc:match([[ <terrain name='$name' tile='$tile'/> ]])
---	parse_children(terrain, doc)
---	return terrain
---end
---
---function Parsers.terraintypes(doc)
---	local terraintypes = doc:match[[
---<terraintypes>
---	{{<terrain name='$name' tile='$tile'/>}}
---</terraintypes>
---	]]
---	parse_children(terraintypes, doc)
---	return terraintypes
---end
--- ]]..Properties..[[
-
-function Parsers.tileset(doc)
-	local tileset = doc:match[[
-<tileset firstgid='$firstgid' source='$source' name='$name'
-tilewidth='$tilewidth' tileheight='$tileheight' spacing='$spacing'
-margin='$margin' tilecount='$tilecount' columns='$columns'>
-	<tileoffset x='$tileoffsetx' y='$tileoffsety'/>
-	<grid orientation='$gridorient' width='$gridwidth' height='$gridheight'/>
-</tileset>
-	]]
-
-	parseChildren(tileset, doc, function(tileset, tag, parsed)
-		if tag == "tile" then
-			local tileid = tonumber(parsed.id) or parsed.id
-			tileset[tileid] = parsed
-		else
-			tileset[tag] = parsed
-		end
-	end)
-	return tileset
-end
-
-function Parsers.layer(doc)
-	local layer = doc:match([[
-<layer id='$id' name='$name' x='$x' y='$y' width='$width' height='$height'
-opacity='$opacity' visible='$visible' offsetx='$offsetx' offsety='$offsety'/>
-	]])
-	parseChildren(layer, doc)
-	return layer
+	end
+	if not font then
+		local defaultfont = "defaultfont"..pixelsize
+		font = loaded[defaultfont] or LG.newFont(pixelsize)
+		loaded[defaultfont] = font
+	end
+	font:setFilter("nearest", "nearest")
+	node.font = font
+	return node
 end
 
 local function decodeData(gids, data, encoding, compression)
@@ -183,147 +128,168 @@ local function decodeData(gids, data, encoding, compression)
 	end
 end
 
-function Parsers.chunk(doc)
-	return doc:match([[
-<chunk x='$x' y='$y' width='$width' height='$height'>$data</chunk>
-	]])
+function load.tile(node, parent, dir)
+	local gid = node.gid
+	if gid then
+		parent[#parent + 1] = gid
+	end
+	return node
 end
 
-function Parsers.data(doc)
-	local data = doc:match([[
-<data encoding='$encoding' compression='$compression'/>
-	]])
-	local encoding = data.encoding
-	local compression = data.compression
-	parseChildren(data, doc, function(data, tag, parsed)
-		if parsed then
-			if tag == "chunk" then
-				data[#data + 1] = parsed
-				decodeData(parsed, parsed.data,
-					encoding, compression)
-				parsed.data = nil
-			else
-				data[tag] = parsed
+function load.data(node, parent, dir)
+	local encoding = node.encoding
+	local compression = node.compression
+	if encoding or compression then
+		local data = node[1]
+		node[1] = nil
+		if type(data) == "string" then
+			decodeData(node, data, encoding, compression)
+		else
+			for i = 1, #node do
+				data = node[i]
+				decodeData(data, data, encoding, compression)
 			end
-		end
-	end)
-	local datadata = doc:match("<data>$1</data>")
-	datadata = datadata and datadata[1]
-	decodeData(data, datadata, encoding, compression)
-	return data
-end
-
-function Parsers.imagelayer(doc)
-	local layer = doc:match([[
-<imagelayer id='$id' name='$name' offsetx='$offsetx' offsety='$offsety'
-x='$x' y='$y' opacity='$opacity' visible='$visible'/>
-	]])
-	parseChildren(layer, doc)
-	return layer
-end
-
-function Parsers.group(doc)
-	local group = doc:match([[
-<group id='$id' name='$name' offsetx='$offsetx' offsety='$offsety'
-opacity='$opacity' visible='$visible'/>
-	]])
-	parseChildren(group, doc, function(group, tag, parsed)
-		if parsed then
-			if tag == "layer"
-			or tag == "objectgroup"
-			or tag == "imagelayer"
-			or tag == "group"
-			then
-				group[#group + 1] = parsed
-			else
-				group[tag] = parsed
-			end
-		end
-	end)
-	return group
-end
-
-function Parsers.map(doc)
-	local map = doc:match([[
-<map version='$version' tiledversion='$tiledversion' orientation='$orientation'
-renderorder='$renderorder' width='$width' height='$height'
-tilewidth='$tilewidth' tileheight='$tileheight' hexsidelength='$hexsidelength'
-staggeraxis='$staggeraxis' staggerindex='$staggerindex'
-backgroundcolor='$backgroundcolor' nextlayerid='$nextlayerid'
-nextobjectid='$nextobjectid'/>
-	]])
-
-	local tilesets = {}
-	map.tilesets = tilesets
-	parseChildren(map, doc, function(map, tag, parsed)
-		if parsed then
-			if tag == "layer"
-			or tag == "objectgroup"
-			or tag == "imagelayer"
-			or tag == "group"
-			then
-				map[#map + 1] = parsed
-			elseif tag == "tileset" then
-				tilesets[#tilesets + 1] = parsed
-			else
-				map[tag] = parsed
-			end
-		end
-	end)
-	return map
-end
-
-function Parsers.objecttypes(doc)
-	local objecttypes = doc:match([[
-<objecttypes>
-{{<objecttype name="$_">
-{{<property name="$_" type="$type" default="$default"/>}}
- </objecttype>}}
-</objecttypes>
-	]])
-	for tname, properties in pairs(objecttypes) do
-		for pname, property in pairs(properties) do
-			properties[pname] = property.default
 		end
 	end
-	return objecttypes
+	return node
 end
 
-local function parseValue(key, value, dir)
-	local number = tonumber(value)
-	if number then
-		return number
-	elseif value == "" then
-		return nil
-	elseif value == "false" then
-		return false
-	elseif value == "true" then
-		return true
+function load.tileoffset(node, parent, dir)
+	parent.tileoffsetx = node.x
+	parent.tileoffsety = node.y
+end
+
+function load.grid(node, parent, dir)
+	parent.gridwidth = node.width
+	parent.gridheight = node.height
+	parent.gridorientation = node.orientation
+end
+
+function load.image(node, parent, dir)
+	local file = dir..node.source
+	local image = loaded[file]
+	if not image then
+		image = LG.newImage(file)
+		if image then
+			image:setFilter("nearest", "nearest")
+		end
+		loaded[file] = image
+	end
+	parent.image = image
+end
+
+function load.tileset(node, parent, dir)
+	local source = node.source
+	if source then
+		local file = dir..source
+		local exttileset = loaded[file]
+		if not exttileset then
+			exttileset = Tiled.load(file)
+			loaded[file] = exttileset
+		end
+		tablex.update(node, exttileset)
 	end
 
-	if type(value)=="table" then
-		if key == "image" then
-			local file = dir..value.source
-			return loaded[file] or (LFS.getInfo(file) and LG.newImage(file))
+	local tilecount = node.tilecount or 0
+	local columns = node.columns or 0
+	local rows = floor(tilecount/columns)
+	local tilewidth = node.tilewidth
+	local tileheight = node.tileheight
+	local image = node.image
+	local imagewidth = image:getWidth()
+	local imageheight = image:getHeight()
+	local i, x, y = 0, 0, 0
+	for r = 1, rows do
+		for c = 1, columns do
+			local tile = node[i] or {}
+			node[i] = tile
+			tile.tileset = node
+			tile.quad = LG.newQuad(x, y, tilewidth, tileheight,
+						imagewidth, imageheight)
+			x = x + tilewidth
+			i = i + 1
 		end
+		x = 0
+		y = y + tileheight
+	end
 
-		local properties = value.properties
-		if properties then
-			for k, v in pairs(properties) do
-				value[k] = parseValue(k, v.value, dir)
+	if parent then
+		local tilesets = parent.tilesets or {}
+		parent.tilesets = tilesets
+		tilesets[#tilesets + 1] = node
+		return
+	end
+
+	return node
+end
+
+function load.map(node, parent, dir)
+	local tilesets = node.tilesets or {}
+	local tiles = {}
+	node.tiles = tiles
+	for i = 1, #tilesets do
+		local tileset = tilesets[i]
+		local gid = tileset.firstgid
+		local tile = tileset[0]
+		if tile then
+			tiles[gid] = tile
+			gid = gid + 1
+		end
+		for t = 1, #tileset do
+			local tile = tileset[t]
+			tiles[gid] = tile
+			gid = gid + 1
+		end
+	end
+	return node
+end
+
+local function loadRecursive(doc, parent, dir)
+	if type(doc) == "string" then
+		return doc
+	end
+	local node = {}
+	local tag = doc.tag
+	node.tag = tag
+	local attr = doc.attr
+	for k,v in pairs(attr) do
+		local number = tonumber(v)
+		if number then
+			v = number
+		elseif v == "" then
+			v = nil
+		elseif v == "false" then
+			v = false
+		elseif v == "true" then
+			v = true
+		end
+		node[k] = v
+	end
+	local n = #doc
+	for i = 1, n do
+		node[i] = loadRecursive(doc[i], node, dir)
+	end
+	node = load[tag](node, parent, dir)
+	if not node then
+		return
+	end
+	for gap2 = n-1, 1, -1 do
+		if not node[gap2] then
+			for gap1 = gap2-1, 0, -1 do
+				if gap1 == 0 or node[gap1] then
+					local gapsize = gap2 - (gap1 + 1)
+					for i = gap2+1, n do
+						gap1 = gap1 + 1
+						node[gap1] = node[i]
+						node[i] = nil
+					end
+					n = n - gapsize
+					break
+				end
 			end
 		end
-		value.properties = nil
-
-		for k, v in pairs(value) do
-			value[k] = parseValue(k, v, dir)
-		end
-
-		if not next(value) then
-			return nil
-		end
 	end
-	return value
+	return node
 end
 
 function Tiled.load(file)
@@ -339,144 +305,139 @@ function Tiled.load(file)
 		return
 	end
 
-	local dir = dirname(file) or ""
-	local parser = Parsers[doc.tag]
-	local tbl = parser and parser(doc)
-	tbl = parseValue(doc.tag, tbl, dir)
-
-	local tilesets = tbl.tilesets
-	if tilesets then
-		local tiles = {}
-		tbl.tiles = tiles
-		for i = 1, #tilesets do
-			local tileset = tilesets[i]
-			local source = tileset.source
-			if source then
-				local tsxfile = dir..source
-				local exttileset = Tiled.load(tsxfile)
-				if exttileset then
-					tablex.update(tileset, exttileset)
-				end
-			end
-			local image = tileset.image
-			image:setFilter("nearest", "nearest")
-			local imagewidth = 0
-			local imageheight = 0
-			if image then
-				imagewidth = image:getWidth()
-				imageheight = image:getHeight()
-			end
-			local tilecount = tileset.tilecount or 0
-			local columns = tileset.columns or 0
-			local rows = floor(tilecount/columns)
-			local i, x, y = 0, 0, 0
-			local gid = tileset.firstgid
-			local tilewidth = tileset.tilewidth
-			local tileheight = tileset.tileheight
-			for r = 1, rows do
-				for c = 1, columns do
-					local tile = tileset[i] or {}
-					tiles[gid] = tile
-					tile.tileset = tileset
-					tile.gid = gid
-					tile.quad = LG.newQuad(x, y,
-						tilewidth, tileheight,
-						imagewidth, imageheight)
-					x = x + tilewidth
-					i = i + 1
-					gid = gid + 1
-				end
-				x = 0
-				y = y + tileheight
-			end
-		end
-	end
-
-	return tbl
+	local dir = file:match('(.*/)[^/]*$') or ""
+	return loadRecursive(doc, nil, dir)
 end
 
-local function draw(elem, mapelem)
-	if elem.visible == false then
-		return
-	end
-
-	local x = elem.x or 0
-	local y = elem.y or 0
-	local offsetx = elem.offsetx or 0
-	local offsety = elem.offsety or 0
-	local width = elem.width
-	local height = elem.height
-	local rotation = elem.rotation or 0
-	local scalex, scaley = 1, 1
-	local tilewidth
-	local tileheight
-
-	local gid = elem.gid
-	local tile
-	local tileset
-
-	mapelem = mapelem or elem
-	local tiles = mapelem.tiles
-
-	if gid then
-		tile = tiles[gid]
-	end
-
-	if tile then
-		tileset = tile.tileset
-		tilewidth = tileset.tilewidth
-		tileheight = tileset.tileheight
-		if width ~= tilewidth or height ~= tileheight then
-			scalex = width / tilewidth
-			scaley = height / tileheight
-		end
-	end
-
-	LG.push("transform")
+local transform = {}
+local function transform_default(node, parent, root)
+	local x = node.x or 0
+	local y = node.y or 0
+	local offsetx = node.offsetx or 0
+	local offsety = node.offsety or 0
+	local rotation = node.rotation or 0
 	LG.translate(x + offsetx, y + offsety)
 	LG.rotate(rad(rotation))
-	LG.scale(scalex, scaley)
+end
+setmetatable(transform, {
+	__index = function()
+		return transform_default
+	end
+})
 
-	local data = elem.data
-
-	if data then
-		local maptilewidth = mapelem.tilewidth
-		local maptileheight = mapelem.tileheight
-		local i = 1
-		local x, y = 0, 0
-		for r = 1, height do
-			for c = 1, width do
-				local tile = tiles[data[i]]
-				if tile then
-					local tileset = tile.tileset
-					local tileoffsetx = tileset.tileoffsetx or 0
-					local tileoffsety = tileset.tileoffsety or 0
-					LG.draw(tileset.image, tile.quad, x, y, 0, 1, 1,
-						-tileoffsetx, -tileoffsety)
-				end
-				i = i + 1
-				x = x + maptilewidth
-			end
-			x = 0
-			y = y + maptileheight
+function transform.object(node, parent, root)
+	transform_default(node, parent, root)
+	local gid = node.gid
+	if gid then
+		local maptiles = root.tiles
+		local tile = maptiles[gid]
+		if tile then
+			local tileset = tile.tileset
+			local tilewidth = tileset.tilewidth
+			local tileheight = tileset.tileheight
+			local width = node.width
+			local height = node.height
+			local sx, sy = width/tilewidth, height/tileheight
+			--LG.scale(sx, sy)
 		end
 	end
+end
 
-	if tile then
-		local tileoffsetx = tileset.tileoffsetx or 0
-		local tileoffsety = tileset.tileoffsety or 0
-
-		LG.draw(tileset.image, tile.quad, 0, 0, 0, 1, 1,
-			-tileoffsetx, tileheight - tileoffsety)
+local draw = {}
+setmetatable(draw, {
+	__index = function()
+		return function()
+			return false
+		end
 	end
+})
 
-	for i = 1, #elem do
-		draw(elem[i], mapelem)
+function draw.data(node, parent, root)
+	local maptiles = root.tiles
+	if not maptiles then
+		return
 	end
+	local width = node.width or parent.width
+	local height = node.height or parent.height
+	local maptilewidth = root.tilewidth
+	local maptileheight = root.tileheight
+	local i = 1
+	local x, y = 0, 0
+	for r = 1, height do
+		for c = 1, width do
+			local tile = maptiles[node[i]]
+			if tile then
+				local tileset = tile.tileset
+				local tileoffsetx = tileset.tileoffsetx or 0
+				local tileoffsety = tileset.tileoffsety or 0
+				LG.draw(tileset.image, tile.quad, x, y, 0, 1, 1,
+					-tileoffsetx, -tileoffsety)
+			end
+			i = i + 1
+			x = x + maptilewidth
+		end
+		x = 0
+		y = y + maptileheight
+	end
+	return true
+end
+draw.chunk = draw.data
 
+function draw.text(node, parent, root)
+	local wrap = node.wrap
+	local width = parent.width
+	local halign = node.halign
+	LG.printf(node.string, node.font, 0, 0, wrap and width, halign)
+	return true
+end
+
+function draw.object(node, parent, root)
+	local gid = node.gid
+	if gid then
+		local maptiles = root.tiles
+		local tile = maptiles[gid]
+		if tile then
+			local width = node.width
+			local tileset = tile.tileset
+			local tileheight = tileset.tileheight
+			local tileoffsetx = tileset.tileoffsetx or 0
+			local tileoffsety = tileset.tileoffsety or 0
+
+			LG.draw(tileset.image, tile.quad, 0, 0, 0, 1, 1,
+				-tileoffsetx, tileheight - tileoffsety)
+		end
+		return true
+	elseif node.ellipse then
+		local x = node.x
+		local y = node.y
+		local width = node.width
+		local height = node.height
+		LG.ellipse("line", x/2, y/2, width/2, height/2)
+		return true
+	elseif node.rectangle then
+		local width = node.width or 0
+		local height = node.height or 0
+		LG.rectangle("line", 0, 0, width, height)
+		return true
+	end
+end
+
+local function drawRecursive(node, parent, root)
+	if node.visible == 0 then
+		return
+	end
+	root = root or node
+	local tag = node.tag
+	LG.push("transform")
+	transform[tag](node, parent, root)
+	if not draw[tag](node, parent, root) then
+		for i = 1, #node do
+			drawRecursive(node[i], node, root)
+		end
+	end
 	LG.pop()
 end
-Tiled.draw = draw
+Tiled.draw = drawRecursive
 
 function Tiled.unload()
 	loaded = {}
