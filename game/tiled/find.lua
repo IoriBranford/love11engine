6116f842b7af0d4f490490763a90cb734a410c1c
IoriBranford <ioribranford@gmail.com>
2020-02-15 00:26:39 -0800

    Make node search breadth-first. Add simple name search.

--- game/tiled/find.lua
+++ game/tiled/find.lua
@@ -1,17 +1,38 @@
+local type = type
 local find = {}
 
 local function _find(node, condition, ...)
+	if type(node) ~= "table" or type(condition) ~= "function" then
+		return
+	end
 	if condition(node, ...) then
 		return node
 	end
 	for i = 1, #node do
-		local found = _find(node[i], condition, ...)
+		local child = node[i]
+		if type(child) == "table" then
+			if condition(child, ...) then
+				return child
+			end
+		end
+	end
+	for i = 1, #node do
+		local child = node[i]
+		local found = _find(child, condition, ...)
 		if found then
 			return found
 		end
 	end
 end
 find.custom = _find
+
+local function named(node, name)
+	return node.name == name
+end
+
+function find.named(node, name)
+	return _find(node, named, name)
+end
 
 local function layerNamed(node, name)
 	return node.tag == "layer" and node.name == name
