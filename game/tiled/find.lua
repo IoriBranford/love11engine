3843779c59e4464e6090e5d6aca85b1134597761
IoriBranford <ioribranford@gmail.com>
2020-02-16 13:47:39 -0800

    Proper breadth-first search. Find by name regex matching.

--- game/tiled/find.lua
+++ game/tiled/find.lua
@@ -1,28 +1,40 @@
 local type = type
 local find = {}
 
+local _find_queue = {}
 local function _find(node, condition, ...)
 	if type(node) ~= "table" or type(condition) ~= "function" then
 		return
 	end
-	if condition(node, ...) then
-		return node
-	end
-	for i = 1, #node do
-		local child = node[i]
-		if type(child) == "table" then
-			if condition(child, ...) then
-				return child
+
+	local q1 = 1
+	local qn = 1
+	_find_queue[1] = node
+	local found
+
+	while q1 <= qn do
+		node = _find_queue[q1]
+		q1 = q1 + 1
+
+		if condition(node, ...) then
+			found = node
+			break
+		end
+
+		for i = 1, #node do
+			local child = node[i]
+			if type(child) == "table" then
+				qn = qn + 1
+				_find_queue[qn] = child
 			end
 		end
 	end
-	for i = 1, #node do
-		local child = node[i]
-		local found = _find(child, condition, ...)
-		if found then
-			return found
-		end
+
+	for i = #_find_queue, 1, -1 do
+		_find_queue[i] = nil
 	end
+
+	return found
 end
 find.custom = _find
 
@@ -32,6 +44,13 @@ end
 
 function find.named(node, name)
 	return _find(node, named, name)
+end
+
+function find.nameRegex(node, pattern)
+	return _find(node, function(node, pattern)
+		local name = node.name
+		return name and name:find(pattern)
+	end, pattern)
 end
 
 local function layerNamed(node, name)
